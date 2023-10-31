43e542d9775cdefba43e5de3872057eb1e3d7bf4
tylersmith <tyjsmith1@gmail.com>
2023-10-30 19:53:27 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+    return `This Saturday, I want to roller-skate!`;
+    } else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+saturdayFun('bathe my dog');
+saturdayFun();
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+    return `This Monday, I will go to the office.`;
+    } else {
+        return `This Monday, I will ${activity}.`;
+    }
+}
+
+mondayWork('work from home');
+mondayWork();
+
+function wrapAdjective(flair) {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("*");
+
+encouragingPromptFunction("awesome");
+encouragingPromptFunction();
